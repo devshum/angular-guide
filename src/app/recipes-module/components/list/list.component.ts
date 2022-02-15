b75fc21a9f9a21dca70e329ceb5f80d74c03b841
devshum <57689318+devshum@users.noreply.github.com>
2022-02-15 12:23:51 +0200

    recipe-interface

--- src/app/recipes-module/components/list/list.component.ts
+++ src/app/recipes-module/components/list/list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from 'src/app/core/models/recipe.interface';
 
 @Component({
   selector: 'app-list',
@@ -6,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list.component.scss']
 })
 export class ListComponent implements OnInit {
+  public recipes: Recipe[] = [
+    {
+      name: 'Test recipe',
+      description: 'Test description',
+      imageUrl: 'https://thumbs.dreamstime.com/b/gourmet-tasty-italian-penne-pasta-plate-close-up-spicy-tomato-herbs-white-served-top-wooden-table-58667798.jpg'
+    }
+  ];
 
   constructor() { }
 
