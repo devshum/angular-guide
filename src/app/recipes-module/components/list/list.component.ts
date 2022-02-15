00bced74e4f656199726cf2b8a85329188727c81
devshum <57689318+devshum@users.noreply.github.com>
2022-02-15 12:44:30 +0200

    recipe-details

--- src/app/recipes-module/components/list/list.component.ts
+++ src/app/recipes-module/components/list/list.component.ts
@@ -12,6 +12,11 @@ export class ListComponent implements OnInit {
       name: 'Test recipe',
       description: 'Test description',
       imageUrl: 'https://thumbs.dreamstime.com/b/gourmet-tasty-italian-penne-pasta-plate-close-up-spicy-tomato-herbs-white-served-top-wooden-table-58667798.jpg'
+    },
+    {
+      name: 'Test recipe',
+      description: 'Test description',
+      imageUrl: 'https://thumbs.dreamstime.com/b/gourmet-tasty-italian-penne-pasta-plate-close-up-spicy-tomato-herbs-white-served-top-wooden-table-58667798.jpg'
     }
   ];
 
