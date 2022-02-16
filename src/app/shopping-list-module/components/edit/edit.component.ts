9877a7addb30b23fe79fa5f7e77f5aa816531998
devshum <57689318+devshum@users.noreply.github.com>
2022-02-16 20:35:33 +0200

    add-ingredients

--- src/app/shopping-list-module/components/edit/edit.component.ts
+++ src/app/shopping-list-module/components/edit/edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, Output, ViewChild, EventEmitter } from '@angular/core';
+import { Ingredient } from 'src/app/core/models/ingredient.interface';
 
 @Component({
   selector: 'app-edit',
@@ -6,10 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit.component.scss']
 })
 export class EditComponent implements OnInit {
+  @ViewChild('nameInput') nameInputRef: ElementRef;
+  @ViewChild('amountInput') amountInputRef: ElementRef;
+  @Output() ingredientAdded: EventEmitter<Ingredient> = new EventEmitter<Ingredient>();
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  public onAddItem() {
+    const newIngredient = { name: this.nameInputRef.nativeElement.value, 
+                            amount: this.amountInputRef.nativeElement.value}
+
+    this.ingredientAdded.emit(newIngredient)
+  }
+
+  public onDeleteItem() {
+
+  }
+
+  public onClearItem() {
+
+  }
 }
