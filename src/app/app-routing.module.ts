4bba87cbb8efc4fb0c7ae4bd4cedb0a69bc47959
devshum <57689318+devshum@users.noreply.github.com>
2022-02-19 20:25:06 +0200

    set-routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
+import { ShoppingListComponent } from './shopping-list-module/containers/shopping-list/shopping-list.component';
+import { RecipesComponent } from './recipes-module/containers/recipes/recipes.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: RecipesComponent },
+  { path: 'recipes', component: RecipesComponent },
+  { path: 'shopping-list', component: ShoppingListComponent }
+];
 
 @NgModule({
   imports: [
