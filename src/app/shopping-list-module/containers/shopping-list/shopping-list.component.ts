f9ceb2766ac727a5ddaa6ddc8ec49094f101372b
devshum <57689318+devshum@users.noreply.github.com>
2022-02-27 18:05:42 +0200

    improve-reactive-service

--- src/app/shopping-list-module/containers/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list-module/containers/shopping-list/shopping-list.component.ts
@@ -1,21 +1,29 @@
 import { ShoppingListService } from './../../../core/services/shopping-list.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Ingredient } from 'src/app/core/models/ingredient.interface';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.scss']
 })
-export class ShoppingListComponent implements OnInit {
+export class ShoppingListComponent implements OnInit, OnDestroy {
   public ingredients: Ingredient[];
+  private _unsubscribe = new Subject();
 
   constructor(private _shoppingService: ShoppingListService) { }
 
   ngOnInit(): void {
     this.ingredients = this._shoppingService.getIngredients();
-    this._shoppingService.ingredientsChanges.subscribe((ingredients: Ingredient[]) => {
+    this._shoppingService.ingredientsChanges.pipe(takeUntil(this._unsubscribe))
+                                            .subscribe((ingredients: Ingredient[]) => {
       this.ingredients = ingredients;
     })
+  }
+
+  ngOnDestroy(): void {
+    this._unsubscribe.next();
   }
 }
