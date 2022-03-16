8c40615a45f70e6e887b4ec3ba8e085130820f46
devshum <57689318+devshum@users.noreply.github.com>
2022-03-16 17:31:17 +0200

    set-backend

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiUrl: 'https://recipe-book-1b231-default-rtdb.europe-west1.firebasedatabase.app/'
 };
 
 /*
